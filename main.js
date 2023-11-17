8bcf6189f051f752dc69ca68371ea1359ce499d2
Thomas66 <thomassalvaterra2001@gmail.com>
2023-11-17 15:52:26 +0100

    first version

---
+++ main.js
@@ -0,0 +1,39 @@
+const{ createApp } = Vue;
+
+const opzioni = {
+    data: function () {
+        return {
+            toDo: [{
+                text: "Clean your shoes",
+                done: true,
+                },
+                {
+                text: "Go to work",
+                done: true,
+                },
+                {
+                text: "Eat your protein",
+                done: false,
+                },
+                {
+                text: "Play football",
+                done: true,
+                },
+                {
+                text: "Do your homework",
+                done: false,
+                },
+                {
+                text: "Sleep",
+                done: false
+                }
+            ]
+        }
+    }
+};
+
+mounted() {
+    console.log("Pagina caricata");
+}
+
+createApp(opzioni).mount("#app");
